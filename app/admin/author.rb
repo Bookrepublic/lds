23db1062fd132eae0991a18871784aac7a9cd6d6
Matteo Scurati <matteo.scurati@gmail.com>
2015-04-02 17:47:49 +0200

    Add avatar to authors

--- app/admin/author.rb
+++ app/admin/author.rb
@@ -1,12 +1,15 @@
 ActiveAdmin.register Author do
 
-  permit_params :first_name, :last_name, :biography
+  permit_params :first_name, :last_name, :biography, :avatar
 
   form do |f|
     f.inputs 'Author details' do
       f.input :first_name
       f.input :last_name
       f.input :biography
+    end
+    f.inputs 'Author image' do
+      f.input :avatar, as: :file
     end
     f.actions
   end
--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,8 @@ class Author < ActiveRecord::Base
 
   has_many :author_books
   has_many :books, through: :author_books
+    has_attached_file :avatar
+    validates_attachment_content_type :avatar, content_type: /\Aimage/
 
   validates :first_name, :last_name, presence: true
 
