2c9fbc3d259c4a7ae51d155b684a68762e7f6d07
Matteo Scurati <matteo.scurati@gmail.com>
2015-04-05 23:54:12 +0200

    Add a cover image to Post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405103602) do
+ActiveRecord::Schema.define(version: 20150405175101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,8 +122,12 @@ ActiveRecord::Schema.define(version: 20150405103602) do
     t.text     "article"
     t.date     "date"
     t.boolean  "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.string   "cover_image_file_name"
+    t.string   "cover_image_content_type"
+    t.integer  "cover_image_file_size"
+    t.datetime "cover_image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
