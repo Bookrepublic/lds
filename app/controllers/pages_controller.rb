079938a5d396b35b68b8e0492dad0fd4b9c5b606
Matteo Scurati <matteo.scurati@gmail.com>
2015-04-10 00:46:03 +0200

    First work to love design

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,16 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
-  before_filter :all_books
+  before_filter :all_info
 
   private
 
-    def all_books
+    def all_info
       case params[:id]
       when 'home'
         @books = Book.all
+        @authors = Author.all
+        @users = User.all
       end
     end
 end
