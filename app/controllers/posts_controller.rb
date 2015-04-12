db2f42346d3148359f8350159297d6b280e8b9c8
Matteo Scurati <matteo.scurati@gmail.com>
2015-04-12 18:44:49 +0200

    Posts are styleshed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < InheritedResources::Base
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :check_status, only: [:show]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :action_before_authentication, only: [:edit, :update, :destroy]
 
@@ -13,7 +14,14 @@ class PostsController < InheritedResources::Base
   end
 
   def index
-    @posts = Post.published
+    if user_signed_in?
+      @posts = Post.all
+    else
+      @posts = Post.published
+    end
+  end
+
+  def show
   end
 
   private
@@ -26,10 +34,17 @@ class PostsController < InheritedResources::Base
       params.require(:post).permit(:title, :subtitle, :excerpt, :article, :date, :status)
     end
 
+    def check_status
+      if @post.status == true
+        @post = Post.find(params[:id])
+      else
+        redirect_to(posts_path, notice: "Il post che hai cercato di leggere è ancora in bozza. Sollecita l'autore di pubblicarlo.")
+      end
+    end
+
     def action_before_authentication
       if current_user.id != @post.user_id
         redirect_to(posts_path, notice: "Sorry, you can't edit this post")
       end
     end
 end
-
