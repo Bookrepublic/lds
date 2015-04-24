26eff5a8c48f6f3d82a6d545f6382e38a61eacc1
Matteo Scurati <matteo.scurati@gmail.com>
2015-04-24 16:03:59 +0200

    Collections are now usable in ActiveAdmin

--- app/admin/collection.rb
+++ app/admin/collection.rb
@@ -1,5 +1,22 @@
 ActiveAdmin.register Collection do
 
+  index do
+    selectable_column
+    column "Collection Name", sortable: :name do |collection|
+      link_to collection.name, admin_collection_path(collection)
+    end
+    actions
+  end
+
   permit_params :name
+
+  filter :name
+
+  form do |f|
+    f.inputs 'Collection details' do
+      f.input :name
+    end
+    f.actions
+  end
 
 end
