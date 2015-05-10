97125028b1edd03d8bfce4994764d7bb86967ce3
Matteo Scurati <matteo.scurati@gmail.com>
2015-05-10 20:27:02 +0200

    Fixed

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,7 +8,7 @@ class Book < ActiveRecord::Base
   has_many :authors, through: :author_books
   has_many :book_videos, dependent: :destroy
   has_many :videos, through: :book_videos
-  has_attached_file :cover, :styles => { :thumb => "125x188>" }, :convert_options => { :medium => "-quality 90 -strip" }
+  has_attached_file :cover, :styles => { :thumb => "125x188" }, :convert_options => { :thumb => "-quality 90 -strip" }
   validates_attachment_content_type :cover, content_type: /\Aimage/
 
   scope :published, -> {
