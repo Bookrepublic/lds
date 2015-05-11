55379226ff570b0fbf71fafe14d8a0336770c94e
Matteo Scurati <matteo.scurati@gmail.com>
2015-05-11 17:03:27 +0200

    New seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,12 +51,12 @@ update_records(AuthorBook, [
 ])
 
 update_records(Book, [
-  {id: 1, title: 'Un primo libro', description: 'Questo è proprio un bel libro, a detta di tutti', price: 3.99, pages: 100, isbn: '9788812345678', pubblication_date: '2000-04-01', collection_id: 1},
-  {id: 2, title: 'Un secondo libro', description: 'Questo è proprio un bel libro, a detta di tutti. Come il primo in effetti, o forse di più.', price: 4.99, pages: 200, isbn: '9788812345679', pubblication_date: '2001-04-01', collection_id: 1},
-  {id: 3, title: 'Un terzo libro', description: 'Per questo libro, qualcuno grida al capolavoro', price: 5.99, pages: 300, isbn: '9788812345680', pubblication_date: '2001-04-01', collection_id: 1},
-  {id: 4, title: 'Un quarto libro', description: "Un libro di mezzo, in un periodo di pace per l\'autore", price: 6.99, pages: 400, isbn: '9788812345681', pubblication_date: '2002-04-01', collection_id: 1},
-  {id: 5, title: 'Un quinto libro', description: 'Un nuovo inizio? Difficile a dirsi', price: 7.99, pages: 500, isbn: '9788812345682', pubblication_date: '2003-04-01', collection_id: 2},
-  {id: 6, title: 'Un sesto libro', description: 'Forse la fine di una carriera letteraria', price: 8.99, pages: 600, isbn: '9788812345683', pubblication_date: '2004-04-01', collection_id: 2}
+  {id: 1, title: 'Un primo libro', description: 'Questo è proprio un bel libro, a detta di tutti', price: 3.99, pages: 100, isbn: '9788812345678', pubblication_date: '2000-04-01', collection_id: 1, number: 1, public: true},
+  {id: 2, title: 'Un secondo libro', description: 'Questo è proprio un bel libro, a detta di tutti. Come il primo in effetti, o forse di più.', price: 4.99, pages: 200, isbn: '9788812345679', pubblication_date: '2001-04-01', collection_id: 1, number: 2, public: true},
+  {id: 3, title: 'Un terzo libro', description: 'Per questo libro, qualcuno grida al capolavoro', price: 5.99, pages: 300, isbn: '9788812345680', pubblication_date: '2001-04-01', collection_id: 1, number: 3, public: true},
+  {id: 4, title: 'Un quarto libro', description: "Un libro di mezzo, in un periodo di pace per l\'autore", price: 6.99, pages: 400, isbn: '9788812345681', pubblication_date: '2002-04-01', collection_id: 1, number: 4, public: true},
+  {id: 5, title: 'Un quinto libro', description: 'Un nuovo inizio? Difficile a dirsi', price: 7.99, pages: 500, isbn: '9788812345682', pubblication_date: '2003-04-01', collection_id: 2, number: 5, public: true},
+  {id: 6, title: 'Un sesto libro', description: 'Forse la fine di una carriera letteraria', price: 8.99, pages: 600, isbn: '9788812345683', pubblication_date: '2004-04-01', collection_id: 2, number: 6, public: true},
 ])
 
 update_records(Post, [
