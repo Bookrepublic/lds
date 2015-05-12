c15815cb759fed1a56a2e0c42de587403d1a8b91
Matteo Scurati <matteo.scurati@gmail.com>
2015-05-12 11:23:34 +0200

    Filters

--- app/assets/javascripts/site_isotope.js
+++ app/assets/javascripts/site_isotope.js
@@ -1,7 +1,6 @@
 $( function() {
   var qsRegex;
-  
-  // init Isotope
+
   var $container = $('.isotope').isotope({
     itemSelector: '.shelf__item',
     layoutMode: 'fitRows',
@@ -10,15 +9,36 @@ $( function() {
     }
   });
 
-  // use value of search field to filter
   var $quicksearch = $('#quicksearch').keyup( debounce( function() {
     qsRegex = new RegExp( $quicksearch.val(), 'gi' );
     $container.isotope();
   }, 200 ) );
-  
+
+  $('#filters').on( 'click', '.dialog__filter', function() {
+    var $this = $(this);
+    var $buttonGroup = $this.parents('.dialog__filters');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
+    // set filter for group
+    filters[ filterGroup ] = $this.attr('data-filter');
+    // combine filters
+    var filterValue = '';
+    for ( var prop in filters ) {
+      filterValue += filters[ prop ];
+    }
+    // set filter for Isotope
+    $container.isotope({ filter: filterValue });
+  });
+
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
 });
 
-// debounce so filtering doesn't happen every millisecond
 function debounce( fn, threshold ) {
   var timeout;
   return function debounced() {
